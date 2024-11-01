780b71edd89b2a92d10ffd0f99a847924205af3b
Frederick Chon <frederick.chon@gmail.com>
2024-10-31 18:08:25 -0700

    Add shop-data to firebase DB

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,21 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
-import PRODUCTS from '../shop-data.json';
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  /*
+   * Commented out below code since that should only run once to set the values in firestore.
+   * While not preferable to do this on the front-end, it was nice practice to learn how to store values in firestore database!
+   */
+  // useEffect(() => {
+  //   addCollectionAndDocuments('categories', SHOP_DATA);
+  // }, []);
   const value = { products };
 
   return (
